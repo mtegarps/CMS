c8ca7961fc0b180ad074ab0ba1c01c70a766990b
Minardi <nardiminardi20@gmail.com>
2023-10-23 08:50:19 +0700

    dashboard-email

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -16,7 +16,7 @@ const Sidebar = (props) => {
   const sidebar = useRef(null);
   const [cookies] = useCookies(["role", "name"]);
   const [sidebarExpanded, setSidebarExpanded] = useState(false);
-  
+
 
   const [name, setName] = useState("");
 
@@ -79,7 +79,7 @@ const Sidebar = (props) => {
         <nav className="px-4 py-4 lg:px-6">
           <div>
             <ul className="mb-6 flex flex-col gap-1.5">
-              <SidebarLinkGroup
+              {/* <SidebarLinkGroup
                 activeCondition={
                   router.pathname === "/" || router.pathname.includes("dashboard")
                 }
@@ -98,6 +98,66 @@ const Sidebar = (props) => {
                       >
                         Dashboard
                       </Link>
+                    </React.Fragment>
+                  );
+                }}
+              </SidebarLinkGroup> */}
+
+              <SidebarLinkGroup
+                activeCondition={router.pathname.includes("/user-management")}
+              >
+                {(handleClick, open) => {
+                  return (
+                    <React.Fragment>
+                      <Link
+                        href="#"
+                        className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${(router.pathname === "/" ||
+                          router.pathname.includes("dashboard")) &&
+                          "bg-graydark dark:bg-meta-4"
+                          }`}
+                        onClick={(e) => {
+                          e.preventDefault();
+                          sidebarExpanded
+                            ? handleClick()
+                            : setSidebarExpanded(true);
+                        }}
+                      >
+                        Dashboard
+                        <svg
+                          className={`absolute right-4 top-1/2 -translate-y-1/2 fill-current ${open && "rotate-180"
+                            }`}
+                          width="20"
+                          height="20"
+                          viewBox="0 0 20 20"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                            d="M4.41107 6.9107C4.73651 6.58527 5.26414 6.58527 5.58958 6.9107L10.0003 11.3214L14.4111 6.91071C14.7365 6.58527 15.2641 6.58527 15.5896 6.91071C15.915 7.23614 15.915 7.76378 15.5896 8.08922L10.5896 13.0892C10.2641 13.4147 9.73651 13.4147 9.41107 13.0892L4.41107 8.08922C4.08563 7.76378 4.08563 7.23614 4.41107 6.9107Z"
+                            fill=""
+                          />
+                        </svg>
+                      </Link>
+                      <div
+                        className={`translate ease-linear
+                         transform overflow-hidden ${!open && "hidden"
+                          }`}
+                      >
+                        <ul className="mt-4 mb-5.5 flex flex-col gap-2.5 pl-6">
+                          <li>
+                            <Link
+                              href="/dashboard/e-mail"
+                              className={`flex items-center space-x-3 ${router.pathname.includes("/dashboard/e-mail") ? "text-white" : ""
+                                }`}
+                            >
+                              <GoLog />
+                              <span>Email</span>
+                            </Link>
+                          </li>
+                        </ul>
+                      </div>
                     </React.Fragment>
                   );
                 }}
