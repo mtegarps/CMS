332f83d9bbc72f1b39d2798ecb9efab3df75c1d3
Minardi <nardiminardi20@gmail.com>
2023-10-24 13:26:47 +0700

    24-10-2023

--- src/pages/dashboard/e-mail/index.jsx
+++ src/pages/dashboard/e-mail/index.jsx
@@ -47,17 +47,18 @@ const Page = () => {
 
           return (
             <div key={index}>
-              <div className="rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
-                <div className="flex h-11.5 w-11.5 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4">
-                  <BiBlanket className="fill-primary dark:fill-white" size={22} />
+              <div className="
+              rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
+                <div className="flex items-center justify-center ">
+                  <BiBlanket className="fill-primary  dark:fill-white" size={22} />
                 </div>
 
-                <div className="mt-4 flex items-end justify-between">
+                <div className="mt-4 flex items-end justify-center">
                   <div>
                     <h4 className="text-title-md font-bold text-black dark:text-white">
-                     {lastIndex === index ? item.totalCampaign : item.statusName}
+                     {lastIndex === index ? 'Total Campaign' : item.statusName}
                     </h4>
-                    <span className="text-sm font-medium">Total</span>
+                    <span className="text-sm font-medium">{lastIndex === index ? ' - ' : 'Total'}</span>
                   </div>
 
                   <span className="flex items-center gap-1 text-sm font-medium text-meta-3">
